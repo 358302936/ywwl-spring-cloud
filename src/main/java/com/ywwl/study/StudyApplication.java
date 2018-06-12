1c268cb15b4d922f3de931b7f0d72718abd55026
yanjun <yanjun@ywwl.com>
2018-06-12 17:15:15 +0800

    增加eureka

--- src/main/java/com/ywwl/study/StudyApplication.java
+++ src/main/java/com/ywwl/study/StudyApplication.java
@@ -1,15 +1,18 @@
 package com.ywwl.study;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @Author yanuun
  * @Date 20:13 2018/5/17
  **/
+@EnableEurekaServer
 @SpringBootApplication
 public class StudyApplication {
     public static void main(String[] args){
-        SpringApplication.run(StudyApplication.class,args);
+        new SpringApplicationBuilder(StudyApplication.class).web(true).run(args);
     }
+
 }
